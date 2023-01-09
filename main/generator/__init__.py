90c614742b02592c705df0fb3d1f1504f5f2156d
Chino Franco <chino.franco@gmail.com>
2023-01-10 00:29:53 +0900

    Updated `create_generator` function docs

--- main/generator/__init__.py
+++ main/generator/__init__.py
@@ -2,7 +2,13 @@ import os
 from .models import MarkdownGenerator
 
 
-def create_generator():
+def create_generator() -> MarkdownGenerator:
+    """
+    Creates an instance of the generator app.
+
+    Returns:
+        MarkdownGenerator: Markdown generator app
+    """
     app = MarkdownGenerator(config="config.yaml")
     print("Created Markdown generator...")
     return app
