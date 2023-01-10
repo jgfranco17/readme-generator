65e004ebf005c158ddd34363ad73bdcd4d16689b
Chino Franco <chino.franco@gmail.com>
2023-01-10 17:44:13 +0900

    Added config parsing

--- generator/__init__.py
+++ generator/__init__.py
@@ -1,6 +1,21 @@
 import os
+import argparse
 from .models import MarkdownGenerator
 
+
+def config():    
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--title", "-t",
+                        type=str,  
+                        default="Project Title", 
+                        help="Title of project")
+    parser.add_argument("--style", "-s",
+                        type=str,
+                        required=True,
+                        help="Choose from \'standard\' or \'minimal\'")
+    args = parser.parse_args()
+    return args
+    
 
 def create_generator() -> MarkdownGenerator:
     """
@@ -9,6 +24,7 @@ def create_generator() -> MarkdownGenerator:
     Returns:
         MarkdownGenerator: Markdown generator app
     """
-    app = MarkdownGenerator(config="config.yaml")
+    app_config = config()
+    app = MarkdownGenerator(title=app_config.title, style=app_config.style)
     print("Created Markdown generator...")
     return app
