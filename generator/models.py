bd0482a5a44f39b2d768c0c32844620b555bf7a2
Chino Franco <chino.franco@gmail.com>
2023-01-10 17:43:34 +0900

    Added `Lorem Ipsum` template text

--- generator/models.py
+++ generator/models.py
@@ -1,34 +1,41 @@
 import os
-import yaml
-from tkinter import Tk, Entry, Button
-from dataclasses import dataclass
-from formats import Header, Paragraph, Badge
+from .formats import Header, Badge, Section
 
 class MarkdownGenerator(object):
-    def __init__(self, title:str="Project Title"):
-        self.root = Tk()
-        self.root.title("Github README Generator")
-        self.entry = Entry(self.root, width=40, borderwidth=5)
-        self.entry.grid(row=0, column=0, columnspan=3, padx=10, pady=10)
+    def __init__(self, title:str, style:str):
         self.project_title = title
+        self.style = style.lower()
+        
+        with open(os.path.join(os.path.dirname(__file__), "template.txt"), "r") as template_file:
+            self.template_text = template_file.read()
+            
+        self.content_titles_options = {
+            "minimal": ["About", "Setting Up", "Usage", "Authors"],
+            "standard": ["About", "Prerequisites", "Usage", "Testing", "Teck Stack", "Authors"]
+        }
+        self.content_titles = self.content_titles_options.get(self.style)
     
-    def write(self) -> None:
-        output_directory = os.path.join(os.path.dirname(__file__), "output") 
+    def write(self) -> str:
+        output_directory = os.path.join(os.getcwd(), "output") 
         os.makedirs(output_directory, exist_ok=True)
         filepath = os.path.join(output_directory, "README.md")
-        print("Writing README file...")
+        add_newline = lambda t: t + "\n\n"
         headings = [
             Header(1, self.project_title).content,
-            " ".join([Badge("status", "active", "brightgreen"), Badge("license", "MIT", "blue")]),
-            
+            " ".join([str(badge) for badge in (Badge("status", "active", "brightgreen"), Badge("license", "MIT", "blue"))]),
+            "---"
         ]
+        contents = [Section(title, self.template_text).content for title in self.content_titles]
         
         with open(filepath, "w") as file:
             for heading in headings:
-                file.write(heading)
+                file.write(add_newline(heading))
+            for content in contents:
+                file.write(add_newline(content))
                 
-            for 
+        print(f'Wrote README file to {output_directory}')
     
     def run(self) -> None: 
-        print("Running generator!")
+        print("Running README generator!")
+        self.write()
         
