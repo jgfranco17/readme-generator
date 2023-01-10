5c33565b7f5f313f34a5fc093cb753db9167ba55
Chino Franco <chino.franco@gmail.com>
2023-01-10 17:07:16 +0900

    Removed config reader

--- generator/models.py
+++ generator/models.py
@@ -2,47 +2,32 @@ import os
 import yaml
 from tkinter import Tk, Entry, Button
 from dataclasses import dataclass
+from formats import Header, Paragraph, Badge
 
 class MarkdownGenerator(object):
-    def __init__(self, config:str="config.yaml"):
-        self.__filename = "README.md"
-        self.__config = self.__read_config_file(config)
+    def __init__(self, title:str="Project Title"):
         self.root = Tk()
         self.root.title("Github README Generator")
         self.entry = Entry(self.root, width=40, borderwidth=5)
         self.entry.grid(row=0, column=0, columnspan=3, padx=10, pady=10)
-        
-    @property
-    def filename(self) -> str:
-        return self.__filename
-    
-    @staticmethod
-    def __read_config_file(filename:str) -> dict:
-        """
-        Read a YAML configuration file and convert to Python dictionary.
-
-        Args:
-            filename (str): File path of YAML configuration file
-
-        Returns:
-            dict: Parsed YAML configuration dictionary
-        """
-        filename = os.path.join(os.path.dirname(__file__), filename)
-        if not os.path.exists(filename):
-            raise ValueError(f'Config file does not exist: {filename}')
-        
-        if not (filename.endswith(".yml") or filename.endswith(".yaml")):
-            raise ValueError("Config file must be a YAML file format!")
-        
-        # Open the YAML config file
-        with open(filename, "r") as file:
-            configs = yaml.load(file, Loader=yaml.FullLoader)
-        
-        print(f'Read config file: {filename}')    
-        return configs
+        self.project_title = title
     
     def write(self) -> None:
-        pass
+        output_directory = os.path.join(os.path.dirname(__file__), "output") 
+        os.makedirs(output_directory, exist_ok=True)
+        filepath = os.path.join(output_directory, "README.md")
+        print("Writing README file...")
+        headings = [
+            Header(1, self.project_title).content,
+            " ".join([Badge("status", "active", "brightgreen"), Badge("license", "MIT", "blue")]),
+            
+        ]
+        
+        with open(filepath, "w") as file:
+            for heading in headings:
+                file.write(heading)
+                
+            for 
     
     def run(self) -> None: 
         print("Running generator!")
